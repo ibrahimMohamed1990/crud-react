9ce8d822b8550c24dddf3c156d670137379011ce
ibrahimMohamed1990 <himamohamed1991@gmail.com>
2020-08-08 01:53:52 +0200

    Updates

---
+++ precache-manifest.dd5eceb9c13129e06d1f20d5c1cbd9c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b555a264205d8fb287279294eab8156",
+    "url": "/crud-react/index.html"
+  },
+  {
+    "revision": "5038474c4244d799eafd",
+    "url": "/crud-react/static/css/main.3b362214.chunk.css"
+  },
+  {
+    "revision": "36318b86a9c99e5391a1",
+    "url": "/crud-react/static/js/2.fb4351b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crud-react/static/js/2.fb4351b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5038474c4244d799eafd",
+    "url": "/crud-react/static/js/main.dc30002e.chunk.js"
+  },
+  {
+    "revision": "c399f3c176fd7034a1a8",
+    "url": "/crud-react/static/js/runtime-main.65db3625.js"
+  }
+]);
